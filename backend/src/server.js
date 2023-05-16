c3f13d908ae93b20da380987caef58fb3897bb42
Andlzap <andlzappa99@gmail.com>
2023-05-16 09:57:44 -0300

    Projeto post-comment

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+console.log(process.env)
+
+const express = require('express')
+
+const app = express()
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Servidor rodando em: ${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`)
+})
